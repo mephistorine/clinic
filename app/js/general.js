ddba0b4e1aae0832f5756be499f59549316fb666
stylesam <stylesam@yandex.ru>
2017-01-10 17:36:25 +0300

    Секция .testimonials завершена

--- app/js/general.js
+++ app/js/general.js
@@ -1,3 +1,4 @@
+$(function () {
 var forEach=function(t,o,r){if("[object Object]"===Object.prototype.toString.call(t))for(var c in t)Object.prototype.hasOwnProperty.call(t,c)&&o.call(r,t[c],c,t);else for(var e=0,l=t.length;l>e;e++)o.call(r,t[e],e,t)};
 
 var hamburgers = document.querySelectorAll(".hamburger");
@@ -8,3 +9,26 @@ var hamburgers = document.querySelectorAll(".hamburger");
 		}, false);
 	});
 }
+
+
+    //array of colors to demonstrate the background filling effect
+    var colors = ["#d35400","#c0392b","#2980b9","#f39c12","#34495e","#3498db"];
+
+    //select random color from the color array
+    var items = Math.floor(Math.random() * colors.length);
+    var color = colors.splice(items, 1);
+
+    //set a random color on document load
+    $(".green input[type='checkbox']:checked + label span").css({"background-color":color});
+    $(".green input[type='radio']:checked + label span").css({"background-color":color});
+
+    //on click switch between background colors
+    $(".green .checkbox").on("click", function(){
+        if($(".green input[type='checkbox']").is(":checked")){
+            $(".green input[type='checkbox']:checked + label span").css({"background-color":"#ecf0f1"});
+        }else{
+            $(".green input[type='checkbox'] + label span").css({"background-color":color});
+        }
+    });
+
+});
